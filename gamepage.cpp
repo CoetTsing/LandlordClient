46a37f16d4e01fd101ef90580b4c5835fad73cdb
CoetTsing <ling10505@163.com>
2020-09-03 18:26:24 +0800

    able to ask

--- gamepage.cpp
+++ gamepage.cpp
@@ -1,12 +1,18 @@
 #include "gamepage.h"
 #include "ui_gamepage.h"
 #include <QDebug>
+#include <algorithm>
 
 GamePage::GamePage(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::GamePage)
 {
     ui->setupUi(this);
+    ui->label_4->hide();
+    ui->label_5->hide();
+    ui->label_6->hide();
+    ui->ask->hide();
+    ui->noask->hide();
     clientSocket = new QTcpSocket(this);
     qint16 port = 8888;
     clientSocket->connectToHost("127.0.0.1", port);
@@ -51,11 +57,52 @@ void GamePage::readInfo() {
                 int a = 1000 * (tmp[i] - '0') + 100 * (tmp[i + 1] - '0') + 10 * (tmp[i + 2] - '0') + (tmp[i + 3] - '0');
                 cards.push_back(a);
             }
-//            qDebug() << cards;
-//            qDebug() << tmp.size();
             showCards();
             tmp = tmp.right(tmp.size() - 69);
+        } else if (*tmp.begin() == '2') {
+            nowPlayer = *(tmp.begin() + 1) - '0';
+            hide();
+            lord = "";
+            for (auto i = tmp.begin() + 2; i != tmp.end(); i++)
+                lord += *i;
+            ask();
+            tmp = tmp.right(tmp.size() - 2 - lord.size());
+        } else if (*tmp.begin() == '3') {
+            lordplayer = *(tmp.begin() + 1) - '0';
+            tmp = tmp.right(tmp.size() - 2);
+        } else if (*tmp.begin() == '4') {
+            for (int i = 1; i <= 12; i += 4) {
+                int a = 1000 * (tmp[i] - '0') + 100 * (tmp[i + 1] - '0') + 10 * (tmp[i + 2] - '0') + (tmp[i + 3] - '0');
+                cardsLord.push_back(a);
+            }
+            showCardsLord();
+            if (playerId == lordplayer) {
+                for (int i = 0; i < 3; i++)
+                    cards.push_back(cardsLord[i]);
+                std::sort(cards.begin(), cards.end(), cmp);
+                showCards();
+            }
+            tmp = tmp.right(tmp.size() - 13);
         }
+    }
+}
+
+bool GamePage::cmp(int a, int b) {
+    return a > b;
+}
+
+void GamePage::showCardsLord() {
+    for (int i = 0; i < 3; i++) {
+        delete labelsLord[i];
+        labelsLord[i] = new QLabel(this);
+        QString path = ":/cards/" + QString::number(cardsLord[i]) + ".png";
+        QPixmap pic(path);
+        pic.scaled(72, 97, Qt::KeepAspectRatio);
+        labelsLord[i]->setScaledContents(true);
+        labelsLord[i]->setPixmap(pic);
+        labelsLord[i]->resize(72, 97);
+        labelsLord[i]->move(464 + i * 100, 30);
+        labelsLord[i]->show();
     }
 }
 
@@ -77,4 +124,108 @@ void GamePage::showCards() {
         labels[i]->move(200 + i * 40, 550);
         labels[i]->show();
     }
+}
+
+void GamePage::hide() {
+    if (playerId == 2) {
+        if (nowPlayer == 1) {
+            ui->label_6->show();
+            ui->label_5->hide();
+            ui->label_4->hide();
+        } else if (nowPlayer == 2) {
+            ui->label_4->show();
+            ui->label_5->hide();
+            ui->label_6->hide();
+        } else {
+            ui->label_5->show();
+            ui->label_4->hide();
+            ui->label_6->hide();
+        }
+    } else {
+        if (nowPlayer == 1) {
+            ui->label_5->show();
+            ui->label_6->hide();
+            ui->label_4->hide();
+        } else if (nowPlayer == 2) {
+            ui->label_6->show();
+            ui->label_5->hide();
+            ui->label_4->hide();
+        } else {
+            ui->label_4->show();
+            ui->label_5->hide();
+            ui->label_6->hide();
+        }
+    }
+}
+
+void GamePage::ask() {
+    if (nowPlayer == playerId) {
+        ui->ask->show();
+        ui->noask->show();
+    }
+}
+
+void GamePage::whoIsLord() {
+    if (playerId == 2) {
+        if (lord[2] == '2')
+            lordplayer = 2;
+        else {
+            if (lord[1] == '1')
+                lordplayer = 1;
+            else
+                lordplayer = 3;
+        }
+    }
+    else {
+        if (lord[2] == '3')
+            lordplayer = 3;
+        else {
+            if (lord[1] == '2')
+                lordplayer = 2;
+            else
+                lordplayer = 1;
+        }
+    }
+    heIsLord();
+}
+
+void GamePage::heIsLord() {
+    QString iAmLord = "3" + QString::number(lordplayer);
+    clientSocket->write(iAmLord.toUtf8().data());
+}
+
+void GamePage::on_ask_clicked()
+{
+    lord += QString::number(playerId);
+    if (lord.size() == 3)
+        whoIsLord();
+    else {
+        if (playerId == 2)
+            nowPlayer = 3;
+        else
+            nowPlayer = 1;
+        hide();
+        QString now = "2" + QString::number(nowPlayer) + lord;
+        clientSocket->write(now.toUtf8().data());
+    }
+    ui->ask->hide();
+    ui->noask->hide();
+}
+
+void GamePage::on_noask_clicked()
+{
+    lord += '0';
+    if (lord.size() == 3)
+        whoIsLord();
+    else {
+        if (playerId == 2)
+            nowPlayer = 3;
+        else
+            nowPlayer = 1;
+        hide();
+        QString now = "2" + QString::number(nowPlayer) + lord;
+        clientSocket->write(now.toUtf8().data());
+    }
+    ui->ask->hide();
+    ui->noask->hide();
 }
--- gamepage.h
+++ gamepage.h
@@ -16,12 +16,21 @@ class GamePage : public QWidget
 public:
     explicit GamePage(QWidget *parent = nullptr);
     ~GamePage();
+    static bool cmp(int a, int b);
+    void showCardsLord();
     void showCards();
+    void hide();
+    void ask();
+    void whoIsLord();
+    void heIsLord();
 
 private:
     Ui::GamePage *ui;
     QTcpSocket* clientSocket = nullptr;
+    QString lord = "";
+    int lordplayer;
     int playerId;
+    int nowPlayer;
     QVector<int> cardsLord;
     QVector<int> cards;
     QVector<QLabel*> labelsLord = QVector<QLabel*>(3);
@@ -29,6 +38,8 @@ private:
 
 private slots:
     void readInfo();
+    void on_ask_clicked();
+    void on_noask_clicked();
 };
 
 #endif // GAMEPAGE_H
